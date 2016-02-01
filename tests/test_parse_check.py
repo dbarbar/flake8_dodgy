0318682ea11243f3888a14d1c65d7d88506bd772
David Barbarisi <davidbarbarisi@gmail.com>
2016-01-31 20:51:42 -0500

    More Travis CI things.

---
+++ tests/test_parse_check.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from flake8_dodgy.flake8_dodgy import parse_check
+
+
+class TestParse_check(TestCase):
+    def test_parse_check(self):
+        input_check = (
+            'the_key',
+            'the_message',
+            ('regexp1', 'regexp2'),
+            any
+        )
+
+        actual = parse_check(input_check)
+
+        expected_check = {
+            'key': 'the_key',
+            'message': 'the_message',
+            'regexps': ('regexp1', 'regexp2'),
+            'condition': any
+        }
+
+        self.assertDictEqual(actual, expected_check)
